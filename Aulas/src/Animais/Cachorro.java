95e0474a2bdf1e966fdebbbf68fcf246a3d67356
katlyndias <148639020+katlyndias@users.noreply.github.com>
2024-01-21 15:23:12 -0300

    Initial commit

---
+++ Aulas/src/Animais/Cachorro.java
@@ -0,0 +1,131 @@
+package Animais;
+
+public class Cachorro extends Animal {
+
+    //atributos
+    static int numeroDeCachorros;
+
+    private int tamanhoDoRabo;
+
+
+    public Cachorro(String nome, String cor, int altura, double peso, int tamanhoDoRabo, String estadoDeEspirito) {
+        super(nome, cor, peso);
+        this.nome = nome;
+        this.cor = cor;
+        this.altura = altura;
+        this.peso = peso;
+        this.tamanhoDoRabo = tamanhoDoRabo;
+        this.estadoDeEspirito = estadoDeEspirito;
+
+        numeroDeCachorros ++;
+    }
+
+
+    //construtores
+
+    public Cachorro(String nome, String cor, double peso, int tamanhoDoRabo) {
+        super(nome, cor, peso);
+        this.tamanhoDoRabo = tamanhoDoRabo;
+    }
+
+
+    //metodos
+
+
+    public static int getNumeroDeCachorros() {
+        return numeroDeCachorros;
+    }
+
+    public static void setNumeroDeCachorros(int numeroDeCachorros) {
+        Cachorro.numeroDeCachorros = numeroDeCachorros;
+    }
+
+    public String getNome(){
+        // aqui consigo especificar as regras para permitir ou não acesso
+        return this.nome;
+    }
+
+    public void setNome(String nome){
+        if(nome.equals("Lily")){
+            this.nome = null;
+        }
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public int getTamanhoDoRabo() {
+        return tamanhoDoRabo;
+    }
+
+    public void setTamanhoDoRabo(int tamanhoDoRabo) {
+        this.tamanhoDoRabo = tamanhoDoRabo;
+    }
+
+    public String getEstadoDeEspirito() {
+        return estadoDeEspirito;
+    }
+
+    public String pegar(){
+        return "Bolinha";
+    }
+
+    public String interagir(String acao){
+
+        switch (acao) {
+            case "carinho" : this.estadoDeEspirito = "feliz"; break;
+            case "vai dormir!" : this.estadoDeEspirito = "bravo"; break;
+            case "pisar na patinha" : this.estadoDeEspirito = "triste"; break;
+            default: this.estadoDeEspirito = "neutro"; break;
+
+        }
+        return this.estadoDeEspirito;
+
+    }
+
+    @Override
+    public void soar() {
+        System.out.println("AU AU!");
+    }
+
+    @Override
+    public String toString() {
+        return "Cachorro{" +
+                "nome='" + nome + '\'' +
+                '}';
+    }
+}
+
+//        if (acao.equals("carinho")){
+//            this.estadoDeEspirito = "feliz";
+//        } else if (acao.equals("vai dormir!")) {
+//            this.estadoDeEspirito = "bravo";
+//        } else if (acao.equals("pisar na patinha")) {
+//            this.estadoDeEspirito = "triste";
+//         } else {
+//            this.estadoDeEspirito = "neutro";
+//        }
+//        return estadoDeEspirito;
+
---
+++ Aulas/src/Lojas/Petshop.java
@@ -0,0 +1,11 @@
+//package Lojas;
+//
+//import Animais.Animal;
+//
+//public class Petshop {
+//
+//    public void darBanho(Animal animal){
+//
+//
+//    }
+//}
